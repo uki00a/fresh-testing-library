7c15534f33bfb546f461a10d00df21d7230dd71f
uki00a <uki00a@gmail.com>
2023-08-27 01:21:56 +0900

    test: `determineRoute`

--- internal/fresh/mod.test.ts
+++ internal/fresh/mod.test.ts
@@ -3,7 +3,11 @@ import { describe, it } from "$std/testing/bdd.ts";
 import type { Has } from "$std/testing/types.ts";
 import { assertType } from "$std/testing/types.ts";
 
-import { determineRouteDestinationKind, freshPathToURLPattern } from "./mod.ts";
+import {
+  determineRoute,
+  determineRouteDestinationKind,
+  freshPathToURLPattern,
+} from "./mod.ts";
 
 describe("$fresh-testing-library/_util", () => {
   describe("freshPathToURLPattern", () => {
@@ -42,6 +46,19 @@ describe("$fresh-testing-library/_util", () => {
           },
         );
       }
+    });
+  });
+
+  describe("determineRoute", () => {
+    it("should return `/` when `manifest` parameter is not provided", () => {
+      const request = new Request("http://localhost:9876/api/users/1234");
+      assertEquals(determineRoute(request), "/");
+    });
+
+    it("should return a `path-to-regexp` pattern based on `manifest`", async () => {
+      const request = new Request("http://localhost:9876/api/users/1234");
+      const { default: manifest } = await import("../../demo/fresh.gen.ts");
+      assertEquals(determineRoute(request, manifest), "/api/users/:id");
     });
   });
 
