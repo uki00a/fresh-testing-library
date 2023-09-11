4ca6bc3086250e565d19a64217ea64392d4da8dc
uki00a <uki00a@gmail.com>
2023-09-12 06:30:21 +0900

    fix: import `jsdom` from the npm registry instead of esm.sh

--- components.test.tsx
+++ components.test.tsx
@@ -1,5 +1,5 @@
 import { cleanup, fireEvent, render, setup } from "$fresh-testing-library";
-import { signal } from "@preact/signals";
+import { computed, signal, useSignal } from "@preact/signals";
 import { assertEquals } from "$std/assert/assert_equals.ts";
 import { assertExists } from "$std/assert/assert_exists.ts";
 import { assertFalse } from "$std/assert/assert_false.ts";
@@ -25,5 +25,40 @@ describe("$fresh-testing-library/components", () => {
     await fireEvent.click(minusOne);
     assertExists(screen.getByText("9"));
     assertFalse(screen.queryByText("10"));
+  });
+});
+
+describe("Changing the `src` of the `img` element", () => {
+  // Regression test for https://github.com/uki00a/fresh-testing-library/issues/22
+  beforeAll(setup);
+  afterEach(cleanup);
+
+  it("should work", async () => {
+    const images = [
+      "https://avatars.githubusercontent.com/u/35212662?v=4",
+      "https://avatars.githubusercontent.com/u/42048915?v=4",
+    ];
+    function Test() {
+      const indexOfCurrentImage = useSignal(0);
+      const currentImage = computed(() => images[indexOfCurrentImage.value]);
+      const switchToNextImage = () => {
+        indexOfCurrentImage.value = indexOfCurrentImage.value +
+          1 % images.length;
+      };
+      return (
+        <>
+          <img src={currentImage.value} />
+          <button type="button" onClick={switchToNextImage}>➡️</button>
+        </>
+      );
+    }
+
+    const screen = render(<Test />);
+    const img = screen.getByRole("img");
+    const button = screen.getByRole("button", { name: "➡️" });
+
+    assertEquals(img.getAttribute("src"), images[0]);
+    await fireEvent.click(button);
+    assertEquals(img.getAttribute("src"), images[1]);
   });
 });
