5dc833675115f6d70ad225803451efd9e63a27b1
uki00a <uki00a@gmail.com>
2023-09-28 08:25:48 +0900

    feat(components): basic support for the Clipboard API

---
+++ deps/jest-clipboard.ts
@@ -0,0 +1,6 @@
+/**
+ * {@link https://github.com/marabesi/jest-clipboard}
+ *
+ * NOTE: `alias` parameter is specified to avoid loading `blob-polyfill`.
+ */
+export { setUpClipboard } from "https://esm.sh/jest-clipboard@0.0.11?pin=v132&no-dts&alias=blob-polyfill:vm";
