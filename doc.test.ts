93d81d185c77a613ab5627ee5269a306bde77d2d
Yuki Tanaka <uki00a@gmail.com>
2023-10-06 05:54:15 +0900

    deps: bump `deno_power_doctest` to v0.3.0 (#37)

--- doc.test.ts
+++ doc.test.ts
@@ -1,17 +1,6 @@
-import { parseMarkdown } from "./deps/deno-power-doctest.ts";
-import { dirname, join } from "node:path";
+import { parseMarkdown, runCodeBlocks } from "./deps/deno-power-doctest.ts";
 
 const readmeURL = new URL(import.meta.resolve("./README.md"));
 const readme = await Deno.readTextFile(readmeURL);
-const baseDir = dirname(readmeURL.pathname);
 const blocks = parseMarkdown(readme);
-for (const x of blocks) {
-  const { range: { start: { line: start }, end: { line: end } } } = x;
-  const filename = join(baseDir, `README\$${start}-${end}.doctest.tsx`);
-  await Deno.writeTextFile(filename, x.code);
-  try {
-    await import(filename);
-  } finally {
-    await Deno.remove(filename);
-  }
-}
+await runCodeBlocks(blocks);
