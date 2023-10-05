9be25b091fc6f44ea26e0f39aef5439a1ef23299
uki00a <uki00a@gmail.com>
2023-10-06 05:51:37 +0900

    deps: bump `deno_power_doctest` to v0.3.0

--- deps/deno-power-doctest.ts
+++ deps/deno-power-doctest.ts
@@ -1,1 +1,2 @@
-export { parse as parseMarkdown } from "https://deno.land/x/deno_power_doctest@0.2.0/markdown.ts";
+export { runCodeBlocks } from "https://deno.land/x/deno_power_doctest@0.3.0/runner.ts";
+export { parse as parseMarkdown } from "https://deno.land/x/deno_power_doctest@0.3.0/markdown.ts";
