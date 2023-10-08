356997ba677b37a0bd1e95b82ec14b75f77d5ecd
uki00a <uki00a@gmail.com>
2023-10-08 10:44:34 +0900

    feat(createHandlerContext): support rendering sync route components

--- demo/routes/(admin)/dashboard.tsx
+++ demo/routes/(admin)/dashboard.tsx
@@ -1,7 +1,21 @@
 import { Head } from "$fresh/runtime.ts";
-import type { PageProps } from "$fresh/server.ts";
+import type { Handlers, PageProps } from "$fresh/server.ts";
 
-export default function AdminDashboard(props: PageProps) {
+interface Data {
+  totalUsers: number;
+  activeUsers: number;
+}
+
+export const handler: Handlers<Data> = {
+  GET(_, ctx) {
+    return ctx.render({
+      totalUsers: 123,
+      activeUsers: 45,
+    });
+  },
+};
+
+export default function AdminDashboard(props: PageProps<Data>) {
   return (
     <>
       <Head>
@@ -11,9 +25,9 @@ export default function AdminDashboard(props: PageProps) {
       <main>
         <dl>
           <dt>Total users</dt>
-          <dd>123</dd>
+          <dd>{props.data.totalUsers}</dd>
           <dt>Active users</dt>
-          <dd>45</dd>
+          <dd>{props.data.activeUsers}</dd>
         </dl>
       </main>
     </>
