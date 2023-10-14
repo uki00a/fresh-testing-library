acfbdcc030be9dc59a0e97cf936c5b290cc99379
uki00a <uki00a@gmail.com>
2023-10-14 20:52:17 +0900

    chore(demo): add `docs/` route

---
+++ demo/routes/docs/[...path].tsx
@@ -0,0 +1,43 @@
+import { Head, Partial } from "$fresh/runtime.ts";
+import type { Handlers, PageProps, RouteConfig } from "$fresh/server.ts";
+import { CSS as GFMCSS, render as renderGFM } from "$gfm";
+import { join } from "node:path";
+
+interface Data {
+  content: string;
+}
+
+export const config: RouteConfig = {
+  skipAppWrapper: true,
+};
+
+export const handler: Handlers<Data> = {
+  async GET(_, ctx) {
+    const url = ctx.params.path === ""
+      ? import.meta.resolve("../../../README.md")
+      : import.meta.resolve(join("../../../docs", `${ctx.params.path}.md`));
+    const md = await Deno.readTextFile(new URL(url));
+    const data: Data = {
+      content: renderGFM(md, {}),
+    };
+    return ctx.render(data);
+  },
+};
+
+export default function DocPage(props: PageProps<Data>) {
+  return (
+    <>
+      <Head>
+        <style id="gfm">{GFMCSS}</style>
+      </Head>
+      <Partial name="docs-content">
+        <article
+          data-color-mode="auto"
+          data-dark-theme="dark"
+          class="p-4 mx-auto w-full markdown-body"
+          dangerouslySetInnerHTML={{ __html: props.data.content }}
+        />
+      </Partial>
+    </>
+  );
+}
---
+++ demo/routes/docs/_layout.tsx
@@ -0,0 +1,38 @@
+import type { LayoutProps } from "$fresh/server.ts";
+import { Partial } from "$fresh/runtime.ts";
+
+export default function Layout(props: LayoutProps) {
+  return (
+    <body class="flex">
+      <aside class="min-w-[14rem] p-4">
+        <Sidebar
+          docs={[
+            { title: "TOP", link: "/docs" },
+            { title: "Permissions", link: "/docs/permissions" },
+          ]}
+        />
+      </aside>
+      <main>
+        <props.Component />
+      </main>
+    </body>
+  );
+}
+
+interface SidebarProps {
+  docs: Array<{ title: string; link: string }>;
+}
+
+function Sidebar(props: SidebarProps) {
+  return (
+    <nav f-client-nav>
+      <ul>
+        {props.docs.map((x) => (
+          <li>
+            <a href={x.link}>{x.title}</a>
+          </li>
+        ))}
+      </ul>
+    </nav>
+  );
+}
