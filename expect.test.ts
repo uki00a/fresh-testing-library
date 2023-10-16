05108b01a6b687940f8405c5eeba1cb4a6cc9ae8
uki00a <uki00a@gmail.com>
2023-10-16 09:30:16 +0900

    test(expect): improve test cases

--- expect.test.ts
+++ expect.test.ts
@@ -1,25 +1,34 @@
-import { expect } from "./expect.ts";
+import { expect, fn } from "./expect.ts";
 import { JestAssertionError } from "./deps/expect.ts";
 import { assertExists } from "$std/assert/assert_exists.ts";
 import { assertThrows } from "$std/assert/assert_throws.ts";
-import { fn } from "https://esm.sh/jest-mock@29.7.0?pin=v133";
 
 Deno.test({
   name: "expect",
-  fn: () => {
-    expect(1).toBe(1);
-    assertThrows(() => expect(1).toBe(2), JestAssertionError);
-    assertExists(
-      expect(null).toBeInTheDocument,
-      "`.toBeInTheDocument()` should be defined.",
+  fn: async (t) => {
+    await t.step("basic assertions", () => {
+      expect(1).toBe(1);
+      assertThrows(() => expect(1).toBe(2), JestAssertionError);
+    });
+
+    await t.step(
+      "supports matchers provided by `@testing-library/jest-dom`",
+      () => {
+        assertExists(
+          expect(null).toBeInTheDocument,
+          "`.toBeInTheDocument()` should be defined.",
+        );
+      },
     );
 
-    const inc = fn((n: number) => 1 + n);
-    expect(inc).not.toBeCalled();
-    expect(inc(4)).toBe(5);
-    expect(inc).toBeCalled();
-    expect(inc).toBeCalledTimes(1);
-    expect(inc).toBeCalledWith(4);
+    await t.step("mocks", () => {
+      const inc = fn((n: number) => 1 + n);
+      expect(inc).not.toBeCalled();
+      expect(inc(4)).toBe(5);
+      expect(inc).toBeCalled();
+      expect(inc).toBeCalledTimes(1);
+      expect(inc).toBeCalledWith(4);
+    });
   },
   permissions: "none",
 });
--- testdata/permissions/expect.ts
+++ testdata/permissions/expect.ts
@@ -1,3 +1,4 @@
-import { expect } from "$fresh-testing-library/expect.ts";
+import { expect, fn } from "$fresh-testing-library/expect.ts";
 
 expect(1).toEqual(1);
+expect(fn()).not.toBeCalled();
