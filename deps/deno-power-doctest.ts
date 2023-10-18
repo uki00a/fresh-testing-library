bbdc6fb6e02c24b805922a5bb11652cf6d03709a
Yuki Tanaka <uki00a@gmail.com>
2023-10-18 21:23:20 +0900

    deps: `deno_power_doctest@0.4.0` (#55)

--- deps/deno-power-doctest.ts
+++ deps/deno-power-doctest.ts
@@ -1,2 +1,2 @@
-export { runCodeBlocks } from "https://deno.land/x/deno_power_doctest@0.3.0/runner.ts";
-export { parse as parseMarkdown } from "https://deno.land/x/deno_power_doctest@0.3.0/markdown.ts";
+export { runCodeBlocks } from "https://deno.land/x/deno_power_doctest@0.4.0/runner.ts";
+export { parse as parseMarkdown } from "https://deno.land/x/deno_power_doctest@0.4.0/markdown.ts";
